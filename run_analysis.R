e35d75155fcb0e965693630fb70b81f169bc1d39
Sylvia Nunez <123033847+sylviaanunez@users.noreply.github.com>
2023-01-31 19:29:57 -0800

    tidyDataset

---
+++ run_analysis.R
@@ -0,0 +1,60 @@
+##Merges the training and the test sets to create one data set.
+##Extracts only the measurements on the mean and standard deviation for each measurement. 
+##Uses descriptive activity names to name the activities in the data set
+##Appropriately labels the data set with descriptive variable names. 
+##From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.##
+
+library(dplyr)
+packages <- c("data.table")
+
+dataDescription <- "http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones"
+dataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile = "data.zip")
+unzip("data.zip")
+
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt") #V2 contains label
+features <- read.table("./UCI HAR Dataset/features.txt")               #V2 contains feature labels
+
+##read data
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+
+##combine test and train data sets
+test  <- cbind(subject_test, y_test, X_test)
+train <- cbind(subject_train, y_train, X_train)
+
+fullSet <- rbind(test, train)
+
+##extract mean and sd
+allNames <- c("subject", "activity", as.character(features$V2))
+meanStdColumns <- grep("subject|activity|[Mm]ean|std", allNames, value = FALSE)
+reducedSet <- fullSet[ ,meanStdColumns]
+
+names(activity_labels) <- c("activityNumber", "activityName")
+reducedSet$V1.1 <- activity_labels$activityName[reducedSet$V1.1]
+
+##label
+reducedNames <- allNames[meanStdColumns]    # Names after subsetting
+reducedNames <- gsub("mean", "Mean", reducedNames)
+reducedNames <- gsub("std", "Std", reducedNames)
+reducedNames <- gsub("gravity", "Gravity", reducedNames)
+reducedNames <- gsub("[[:punct:]]", "", reducedNames)
+reducedNames <- gsub("^t", "time", reducedNames)
+reducedNames <- gsub("^f", "frequency", reducedNames)
+reducedNames <- gsub("^anglet", "angleTime", reducedNames)
+names(reducedSet) <- reducedNames  # Apply new names to dataframe
+
+##create tidy data and write table
+tidyDataset <- reducedSet %>% group_by(activity, subject) %>% 
+        summarise_all(funs(mean))
+
+write.table(tidyDataset, file = "tidyDataset.txt", row.names = FALSE)
+
+validate <- read.table("tidyDataset.txt")
+View(validate)
